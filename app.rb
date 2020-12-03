53382675c49c4fb40793561391240c2c7b8be183
Erick Castille <ecastille924@gmail.com>
2020-12-03 23:41:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,8 +32,10 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do 
+    @result = params[:operation]
     @num1 = params[:number1]
     @num2 = params[:number2]
+    
     
     @num1.to_i + @num2.to_i
   end
